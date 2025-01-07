4d5dc289e224254f8e4b926d97bd12a97960ab0f
Diego Alleyne <145381036+diego-game555@users.noreply.github.com>
2025-01-06 21:07:05 -0800

    Waypoints and Rotation
    Finished first part with waypoints and rotation 1/6

--- WaypointsandGraphsP3Alleyne/Assets/Scripts/FollowWP.cs
+++ WaypointsandGraphsP3Alleyne/Assets/Scripts/FollowWP.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class FollowWP : MonoBehaviour
 {
+    public GameObject[] waypoints;
+    int currentWP = 0;
+
+    public float speed = 10.0f;
+    public float rotSpeed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,16 @@ public class FollowWP : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
+            currentWP++;
+
+        if (currentWP >= waypoints.Length)
+            currentWP = 0;
+
+        // this.transform.LookAt(waypoints[currentWP].transform);
+
+        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
 }
